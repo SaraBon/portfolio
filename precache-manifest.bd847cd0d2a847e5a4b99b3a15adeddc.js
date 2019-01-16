a9989d95e52fbef9fa6111b58c3134a582ed36d8
SaraBon <sarabongartz123@gmail.com>
2019-01-16 11:47:49 +0100

    Updates

---
+++ precache-manifest.bd847cd0d2a847e5a4b99b3a15adeddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ada35e5f112ce82b051f",
+    "url": "/portfolio/static/js/main.ada35e5f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/portfolio/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "ada35e5f112ce82b051f",
+    "url": "/portfolio/static/css/main.bb9230ca.chunk.css"
+  },
+  {
+    "revision": "b21c3ec427992b89a66e773befe82264",
+    "url": "/portfolio/index.html"
+  }
+];
